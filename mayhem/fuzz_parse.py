3ab01883b32a9e1c06cbc0e4d6bd676b10603e23
Bailey Capuano <bcapuano@asu.edu>
2023-04-18 19:48:24 -0400

    hide stdout

--- mayhem/fuzz_parse.py
+++ mayhem/fuzz_parse.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
+import io
+from contextlib import contextmanager
 
 import atheris
 import sys
-import networkx as nx
 import fuzz_helpers as fh
+import networkx as nx
 
-with atheris.instrument_imports(include=['pysmiles']):
+with atheris.instrument_imports():
     import pysmiles
+
+
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
 
 
 def exception_handler(exception: Exception, i: int) -> int:
@@ -35,22 +48,23 @@ def TestOneInput(data):
     fdp = fh.EnhancedFuzzedDataProvider(data)
     should_read = fdp.ConsumeBool()
     count += 1
-    try:
-        if should_read:
-            res = pysmiles.read_smiles(fdp.ConsumeRandomString(),
-                                       explicit_hydrogen=fdp.ConsumeBool(),
-                                       zero_order_bonds=fdp.ConsumeBool(),
-                                       reinterpret_aromatic=fdp.ConsumeBool())
-            list(res.nodes)
-        else:
-            mol = nx.Graph()
-            mol.add_edges_from(fh.build_fuzz_list(fdp, [tuple, int]))
-            pysmiles.write_smiles(mol)
-            pysmiles.fill_valence(mol, respect_hcount=fdp.ConsumeBool(),
-                                  respect_bond_order=fdp.ConsumeBool(),
-                                  max_bond_order=fdp.ConsumeIntInRange(0, 10))
-    except Exception as e:
-        exception_handler(e, count)
+    with nostdout():
+        try:
+            if should_read:
+                res = pysmiles.read_smiles(fdp.ConsumeRandomString(),
+                                           explicit_hydrogen=fdp.ConsumeBool(),
+                                           zero_order_bonds=fdp.ConsumeBool(),
+                                           reinterpret_aromatic=fdp.ConsumeBool())
+                list(res.nodes)
+            else:
+                mol = nx.Graph()
+                mol.add_edges_from(fh.build_fuzz_list(fdp, [tuple, int]))
+                pysmiles.write_smiles(mol)
+                pysmiles.fill_valence(mol, respect_hcount=fdp.ConsumeBool(),
+                                      respect_bond_order=fdp.ConsumeBool(),
+                                      max_bond_order=fdp.ConsumeIntInRange(0, 10))
+        except Exception as e:
+            exception_handler(e, count)
 
 
 def main():
